c81dfb6342be5e5fcbc26d0168800cd14e7b75f8
arifur54 <arif.r.92@gmail.com>
2022-03-30 00:20:05 -0400

    User edid and delete functionality added

--- backEnd/index.js
+++ backEnd/index.js
@@ -8,11 +8,13 @@ const postRoute = require("./routes/posts");
 const categoriesRoute = require("./routes/categories");
 const multer = require("multer");
 const cors = require('cors')
+const path = require("path")
 
 PORT = 5000; 
 dotenv.config();
 app.use(express.json());
 app.use(cors())
+app.use("/images", express.static(path.join(__dirname,"/images")))
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
